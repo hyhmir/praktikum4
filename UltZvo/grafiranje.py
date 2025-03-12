33a05f3288255533ac6e2d17e54bd452e0d52394
hyh <samo.krejan@gmail.com>
2025-03-12 16:13:27 +0100

    začel UltZvo 🤓

---
+++ UltZvo/grafiranje.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+import os
+import scienceplots
+
+plt.style.use('science')
+
+files = glob.glob("UltZvo/data/*.csv")
