a21aaac77a0ee147dbe22380e795dc1b8e028992
hyh <samo.krejan@gmail.com>
2025-03-16 13:21:50 +0100

    obdelavu UltZvo🫨

--- UltZvo/grafiranje.py
+++ UltZvo/grafiranje.py
@@ -5,6 +5,22 @@ import glob
 import os
 import scienceplots
 
-plt.style.use('science')
+#plt.style.use('science') #odkomentiraj ko shranjuješ sliko, drgač pa ne ker kr upočasni zadeve
 
 files = glob.glob("UltZvo/data/*.csv")
+files.sort(key=str.lower)
+print(files)
+
+
+file = files[2] #spreminjaj indeks za obdelavo posamezne meritve
+
+print(file)
+
+data = pd.read_csv(file, skiprows=12)
+#print(data)
+plt.plot(data.iloc[:, 0], data.iloc[:, 1])
+
+plt.grid()
+plt.show()
+
+plt.clf()
