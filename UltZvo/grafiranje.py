a3f0bc0b354f81e159abf314098601b5137c01aa
hyh <samo.krejan@gmail.com>
2025-03-16 17:53:22 +0100

    mal delu poročilo - ne da se mi čist 🥲

--- UltZvo/grafiranje.py
+++ UltZvo/grafiranje.py
@@ -20,6 +20,13 @@ data = pd.read_csv(file, skiprows=12)
 #print(data)
 plt.plot(data.iloc[:, 0], data.iloc[:, 1])
 
+# plt.title('Vsi skupaj')   #odkomentiraj za shranjevanje slike
+# plt.xlabel('$U_1 [V]$')
+# plt.ylabel('$I_2 [nA]$')
+# plt.legend()
+# plt.grid()
+# plt.savefig('FraHer/porocilo/vsi.pdf', dpi=1024)
+
 plt.grid()
 plt.show()
 
