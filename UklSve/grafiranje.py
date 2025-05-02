e8f61f494b13aafa2e1b34dd0c725da8afcaa132
hyh <samo.krejan@gmail.com>
2025-05-02 13:54:45 +0200

    naredu grafe za uklon 🤙

--- UklSve/grafiranje.py
+++ UklSve/grafiranje.py
@@ -42,6 +42,8 @@ data10 = data10[(data10['x'] >= -75) & (data10['x'] <= 75)].reset_index(drop=Tru
 N = 1
 
 def fit(x, i, a, b, c):
+    if N ==1:
+        return i*(np.sinc(a * x))**2 + c
     return i*(np.sinc(a * x))**2*(np.sinc(N*b*x)/np.sinc(b*x))**2 + c
 
 
@@ -65,7 +67,7 @@ for par in pari:
     for i, (param, err) in enumerate(zip(popt, perr)):
         print(f"Parameter {i}: {param:.3f} ± {err:.3f}")
 
-    plt.plot(df['x'], df['I'], 'o', label=f'podatki {N}')
+    plt.plot(df['x'], df['I'], 'o', ms=1, label=f'podatki {N}')
     plt.plot(df['x'], fit(df['x'], *popt), 'r-', label=f'fit {N}')
 
     N = par[1]
@@ -78,26 +80,15 @@ for par in pari:
     for i, (param, err) in enumerate(zip(popt, perr)):
         print(f"Parameter {i}: {param:.3f} ± {err:.3f}")
 
-    plt.plot(df['x'], df['I'], 'o', label=f'podatki {N}')
+    plt.plot(df['x'], df['I'], 'o', ms=1, label=f'podatki {N}')
     plt.plot(df['x'], fit(df['x'], *popt), 'grey', label=f'fit {N}')
 
     plt.legend()
     plt.grid()
-    plt.show()
-
-popt, pcov = curve_fit(fit, data1['x'], data1['I'], [0.086, 0.026, 0.097, 0.005], method='lm')#deep seek cooked here
-
-perr = np.sqrt(np.diag(pcov)) 
-print(popt)
-
-print("Fitted parameters with errors:")
-for i, (param, err) in enumerate(zip(popt, perr)):
-    print(f"Parameter {i}: {param:.3f} ± {err:.3f}")
+    plt.xlabel('premik v $x$ smeri od središča [mm]')
+    plt.ylabel('tok $I$ na fotodiodi [A]')
+    plt.savefig(f'UklSve/porocilo/uklon{par[0]}{par[1]}.pdf', dpi=1024)
+    plt.clf()
 
 
-plt.plot(data1['x'], data1['I'], 'o', label='podatki')
-plt.plot(data1['x'], fit(data1['x'], *popt), 'r-', label='fit')
-plt.legend()
-plt.grid()
-plt.title('Interferenčna slika ene in dveh rež')
-plt.show()
+
