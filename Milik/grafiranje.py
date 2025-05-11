0115e359aa9df24684d2bfc304660ad55e9a79a1
hyh <samo.krejan@gmail.com>
2025-05-11 23:15:38 +0200

    data obdelan, treba sam še narisat stvari pa napisat kej lepga

--- Milik/grafiranje.py
+++ Milik/grafiranje.py
@@ -10,3 +10,21 @@ from uncertainties import ufloat
 from uncertainties import unumpy
 
 plt.style.use('science')
+
+nj = 1
+g = 10
+rho1 = 1000
+rho2 = 2
+E = 250
+pi = np.pi
+
+
+data = pd.read_csv('Milik/data/data.csv')
+
+data['v+'] = [ufloat(x, 2) for x in data['v+']]
+data['v-'] = [ufloat(x, 2) for x in data['v-']]
+
+
+data['r'] = unumpy.sqrt(9*nj*(data['v+'] + data['v-'])/4*g*(rho1 - rho2))
+data['ne'] = np.abs((3*pi*data['r']*nj/E)*(data['v+']-data['v-']))
+
